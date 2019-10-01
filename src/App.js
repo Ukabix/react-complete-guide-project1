733e32bb372864c8d69408c2ddf969a0c482ae4a
Ukabix <54454097+Ukabix@users.noreply.github.com>
2019-10-01 19:17:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './person/person';
 class App extends Component {
   state = {
     people: [
-      { name: 'Max', age: 28},
-      { name: 'Manu', age: 29},
-      { name: 'Stephanie', age: 26}
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value'
     
@@ -18,9 +18,9 @@ class App extends Component {
     // this.state.people[0].name = "Maximillian"; //DON'T DO THIS! 
     this.setState( { 
       people: [
-        { name: 'Maximilian', age: 28},
-        { name: 'Manu', age: 29},
-        { name: 'Stephanie', age: 99}
+        { name: 'Maximilian', age: 29 },
+        { name: 'Manu', age: 10 },
+        { name: 'Stephanie', age: 99 }
       ]
     } )
   }
@@ -33,7 +33,7 @@ class App extends Component {
         <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.people[0].name} age={this.state.people[0].age}/>
         <Person name={this.state.people[1].name} age={this.state.people[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.people[2].name} age={this.state.people[0].age}/>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age}/>
       </div>
     );
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'This works now!'));
