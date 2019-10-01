4886dce9169992d2c28bae797403220e00577d10
Ukabix <54454097+Ukabix@users.noreply.github.com>
2019-10-01 18:07:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './person/person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, Im a react app</h1>
+        <p>This is really working</p>
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="29">My Hobbies: Racing</Person>
+        <Person name="Stephanie" age="26"/>
       </div>
     );
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'This works now!'));
   }
 }
 
 export default App;
+
