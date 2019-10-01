03e4bdfa3e506d887601480160e02773865992ee
Ukabix <54454097+Ukabix@users.noreply.github.com>
2019-10-01 19:57:08 +0200

    Update person.js

--- src/person/person.js
+++ src/person/person.js
@@ -3,7 +3,6 @@ import React from 'react';
 const person = (props) => {
     return ( 
     <div>
-        <p>Im {props.name} and I am {props.age} y.o.!</p>
         <p>{props.children}</p>
     </div>
     )
