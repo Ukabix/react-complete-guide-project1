f9f61bb7b388d03bd51e82ad395ef6fcb19d0cb8
Ukabix <54454097+Ukabix@users.noreply.github.com>
2019-10-04 11:31:28 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ class App extends Component {
     
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked! Omg!');
     // this.state.people[0].name = "Maximillian"; //DON'T DO THIS! 
     this.setState( { 
       people: [
-        { name: 'Maximilian', age: 29 },
+        { name: newName, age: 29 },
         { name: 'Manu', age: 10 },
         { name: 'Stephanie', age: 99 }
       ]
@@ -30,10 +30,18 @@ class App extends Component {
       <div className="App">
         <h1>Hi, Im a react app</h1>
         <p>This is really working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.people[0].name} age={this.state.people[0].age}/>
-        <Person name={this.state.people[1].name} age={this.state.people[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.people[2].name} age={this.state.people[2].age}/>
+        <button onClick={() => this.switchNameHandler.bind(this, 'MaxXXX@!')}>Switch Name</button>
+        <Person 
+          name={this.state.people[0].name} 
+          age={this.state.people[0].age}/>
+        <Person 
+          name={this.state.people[1].name} 
+          age={this.state.people[1].age}
+          click={this.switchNameHandler.bind(this, '!!MaXimillian')}
+          >My Hobbies: Racing</Person>
+        <Person 
+          name={this.state.people[2].name} 
+          age={this.state.people[2].age}/>
       </div>
     );
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'This works now!'));
