405c254b87569363368c43b4fa436c73ecb12635
Ukabix <54454097+Ukabix@users.noreply.github.com>
2019-10-04 11:31:33 +0200

    Update person.js

--- src/person/person.js
+++ src/person/person.js
@@ -3,6 +3,7 @@ import React from 'react';
 const person = (props) => {
     return ( 
     <div>
+        <p onClick={props.click}>Im {props.name} and I am {props.age} y.o.!</p>
         <p>{props.children}</p>
     </div>
     )
